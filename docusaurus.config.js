798c82cbe6b54604f5ebcfbf4d64c71f46a47ffe
athom <higeme530@gmail.com>
2024-09-02 12:48:32 +0800

    change social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/geme-social-card.png',
       navbar: {
         // title: 'GEME',
         logo: {
