1a1a3edf944edb4181cdffa79635e51cb630172a
yanyusheng <higeme530@gmail.com>
2024-11-04 14:53:25 +0800

    跟随主题变化设定theme

--- src/components/GiscusComponent/index.js
+++ src/components/GiscusComponent/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Giscus from "@giscus/react";
+import { useColorMode } from "@docusaurus/theme-common"
 
 export default function GiscusComponent() {
+  const { colorMode } = useColorMode();
   return (
     <Giscus
       repo="gemeofficial/gemeblog"
       repoId="R_kgDOMnZijw"
       category="General"
-      categoryId="DIC_kwDOMnZij84Cj89_"  // E.g. id of "General"
-      mapping="url"                        // Important! To map comments to URL
-      term="Welcome to @giscus/react component!"
+      categoryId="DIC_kwDOMnZij84Cj89_"
+      mapping="pathname"
+      term="Participate in Blog interactive comments!"
       strict="0"
       reactionsEnabled="1"
       emitMetadata="1"
       inputPosition="top"
       lang="en"
+      theme={colorMode}
       loading="lazy"
       crossorigin="anonymous"
       async
--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -4,14 +4,13 @@ import BlogPostItem from '@theme-original/BlogPostItem';
 import GiscusComponent from '@site/src/components/GiscusComponent';
 
 // 给博客文章添加评论 
-// 参考链接：https://dev.to/m19v/how-to-add-giscus-comments-to-docusaurus-439h
+// 参考链接：https://rikublock.dev/docs/tutorials/giscus-integration/ 
 export default function BlogPostItemWrapper(props) {
   const { metadata, isBlogPostPage } = useBlogPost()
 
   const { frontMatter, slug, title } = metadata
   // 是否关闭评论 可在blog md的metadata中设置 eg: closeComments: true
   const { closeComments } = frontMatter
-
   return (
     <>
       <BlogPostItem {...props} />
