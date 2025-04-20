cea3b51af5e5f68ff688b8d418eca76941638ed0
athom <higeme530@gmail.com>
2025-04-20 13:33:42 +0800

    allow ip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -190,11 +190,11 @@ const config = {
       ({
         googleAnalytics: {
           trackingID: 'G-0SPG79H0VL',
-          anonymizeIP: true,
+          anonymizeIP: false,
         },
         gtag: {
           trackingID: 'GTM-KB63TRW',
-          anonymizeIP: true,
+          anonymizeIP: false,
         },
         googleTagManager: {
           containerId: 'GTM-KB63TRW',
@@ -580,33 +580,33 @@ const config = {
       algolia: {
         // The application ID provided by Algolia
         appId: '7UN4KF34XB',
-  
+
         // Public API key: it is safe to commit it
         apiKey: '34de1f1b5d45846846f58c27b5aa96fd',
-  
+
         indexName: 'geme-blog-en',
-  
+
         // Optional: see doc section below
         contextualSearch: true,
-  
+
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: 'external\\.com|domain\\.com',
-  
+
         // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
         // replaceSearchResultPathname: {
         //   from: '/docs/', // or as RegExp: /\/docs\//
         //   to: '/',
         // },
-  
+
         // Optional: Algolia search parameters
         searchParameters: {},
-  
+
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
-  
+
         // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
         insights: false,
-  
+
         //... other Algolia params
       },
     }),
