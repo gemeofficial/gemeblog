878fd3d122b2b29ae8ad26dd7168e2b3c3bee6da
Moore <higeme530@gmail.com>
2026-08-19 19:13:17 +0800

    new

--- middleware.js
+++ middleware.js
@@ -2,14 +2,36 @@ export const config = {
     matcher: ['/blog/:path*'],
   };
   
+  const redirects = {
+    '/blog/geme-vs-lomi':
+      'https://gemebio.com/blogs/journal/geme-vs-lomi',
+  
+    '/blog/geme-vs-reencle-composter-2026':
+      'https://gemebio.com/blogs/journal/geme-vs-reencle-composter-2026',
+  
+    '/blog/advanced-geme-compost-application-guide':
+      'https://gemebio.com/blogs/journal/advanced-geme-compost-application-guide',
+  
+    '/blog/electric-compost-bin-filters-costs-comparison':
+      'https://gemebio.com/blogs/journal/electric-compost-bin-filters-costs-comparison',
+  };
+  
   export default function middleware(request) {
     const url = new URL(request.url);
   
-    if (url.pathname === '/blog/geme-vs-lomi') {
+    let pathname = url.pathname;
+  
+    if (pathname.length > 1 && pathname.endsWith('/')) {
+      pathname = pathname.slice(0, -1);
+    }
+  
+    const destination = redirects[pathname];
+  
+    if (destination) {
       return new Response(null, {
         status: 301,
         headers: {
-          Location: 'https://gemebio.com/blogs/journal/geme-vs-lomi',
+          Location: destination,
         },
       });
     }
